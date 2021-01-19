16b3eee5615ec3c15d7c9c0f1346c85f6034085d
amir tavakolian <rezatva@gmail.com>
2021-01-19 17:38:56 +0330

    response class is completed

--- Api/v1/cities/index.php
+++ Api/v1/cities/index.php
@@ -4,12 +4,16 @@ include dirname(__FILE__, 4) . "/" . "autoloader.php";
 
 
 use App\Service\citiesService;
+use \App\Utilities\Response;
 
 $citiesServiceObj = new citiesService();
 $res = $citiesServiceObj->getCitiess();
 
-echo \App\Utilities\Response::responde($res,200,"OK");
+
+// part 8 - file 3.txt ro bekhon  
+// echo Response::responde($res, Response::HTTP_OK);
 
 
+Response::respondeAndDie($res, Response::HTTP_OK);
 
 
